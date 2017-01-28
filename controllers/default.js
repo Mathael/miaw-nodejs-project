929d247a3fd716e0453458fc4ffca7aac806f226
LEBOC Philippe <LebocPhilippe@live.be>
2017-01-28 23:05:30 +0100

    feat(model): add User model to show how to create a model in NodeJS

--- controllers/default.js
+++ controllers/default.js
@@ -1,11 +1,18 @@
 var database = require('../utils/database');
+var User = require('../models/user');
 
 module.exports = function (app,io) {
 
+    /**
+     * @Controller used to test render in NodeJS
+     */
     app.get('/', function (req, res) {
         res.render('default/index.ejs', {page: 1});
     });
 
+    /**
+     * @Controller used to test database query in NodeJS
+     */
     app.get('/test', function (req, res) {
         database.executeQuery('select * from test', function(rows, fields) {
             console.log('rows', rows.length);
@@ -14,5 +21,21 @@ module.exports = function (app,io) {
         });
     });
 
-    console.log('controller 1 loaded');
+    /**
+     * @Controller used to test model in NodeJS
+     */
+    app.get('/user', function (req, res) {
+
+        var user1 = new User('alpha', 'beta', ['ROLE_ADMIN', 'ROLE_USER']);
+        var user2 = new User('alpha2', 'beta2', ['ROLE_USER']);
+
+        console.log(user1);
+        console.log(user2);
+        console.log('user 1 has role ROLE_ADMIN ? : ', user1.hasRole('ROLE_ADMIN'));
+        console.log('user 2 has role ROLE_ADMIN ? : ', user2.hasRole('ROLE_ADMIN'));
+
+        res.render('default/index.ejs', {page: 0});
+    });
+
+    console.log('Default controller loaded successful');
 };
--- models/user.js
+++ models/user.js
@@ -1,4 +1,16 @@
 // Class User
-module.exports = function (){
+var method = User.prototype;
 
+function User(username, password, roles) {
+    this._username = username;
+    this._password = password;
+    this._roles = roles;
+}
+
+method.hasRole = function (role) {
+    return this._roles.filter(function (r) {
+        return r === role;
+    }).length == 1;
 };
+
+module.exports = User;
