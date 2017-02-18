1b9d5b32481f6da191bb08f5d4d694083bcd6d30
LEBOC Philippe <LebocPhilippe@live.be>
2017-02-18 22:34:42 +0100

    fix(controller): cleanup of the default controller

--- controllers/default.js
+++ controllers/default.js
@@ -1,33 +1,20 @@
 var database = require('../utils/database');
-var User = require('../models/user');
 
 module.exports = function (app,io) {
 
     /**
-     * @Controller used to test render in NodeJS
+     * Used to send the main HTML page
      */
     app.get('/', function (req, res) {
         res.render('default/index.ejs');
     });
 
     /**
-     * @Controller used to test database query in NodeJS
-     */
-    app.get('/test', function (req, res) {
-        database.executeQuery('select * from answer', function(rows, fields) {
-            console.log('rows', rows.length);
-            console.log('fields', fields.length);
-            res.render('default/index.ejs', {page: rows.length});
-        });
-    });
-
-    /**
-     * @Controller used to init the database
+     * Used to initialize the database
      */
     app.get('/initdatabase', function (req, res) {
 
-
-        //INSERT  FIRST QUESTION WITH ANSWER
+        //INSERT FIRST QUESTION WITH ANSWER
         database.executeQuery('INSERT INTO question (question,multiple) VALUES ("Aimez vous le NodeJs ?","0")', function(rows, fields) {
             console.log("INSERT success");
         });
@@ -39,8 +26,7 @@ module.exports = function (app,io) {
             console.log("INSERT success");
         });
 
-
-        //INSERT SECOND QUESTION WITH ANSWER
+        //INSERT SECOND QUESTIONS WITH ANSWER
         database.executeQuery('INSERT INTO question (question,multiple) VALUES ("Aimez vous le PHP ?","0")', function(rows, fields) {
             console.log("INSERT success");
         });
@@ -58,23 +44,6 @@ module.exports = function (app,io) {
         database.executeQuery('INSERT INTO user VALUES ("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11","admin","password","ROLE_ADMIN")', function(rows, fields) {
             console.log("INSERT success");
         });
-
-    });
-
-    /**
-     * @Controller used to test model in NodeJS
-     */
-    app.get('/user', function (req, res) {
-
-        var user1 = new User('alpha', 'beta', ['ROLE_ADMIN', 'ROLE_USER']);
-        var user2 = new User('alpha2', 'beta2', ['ROLE_USER']);
-
-        console.log(user1);
-        console.log(user2);
-        console.log('user 1 has role ROLE_ADMIN ? : ', user1.hasRole('ROLE_ADMIN'));
-        console.log('user 2 has role ROLE_ADMIN ? : ', user2.hasRole('ROLE_ADMIN'));
-
-        res.render('default/index.ejs', {page: 0});
     });
 
     app.get('/room/create', function (req, res) {
